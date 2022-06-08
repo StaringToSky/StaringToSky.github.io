3e4d4c5151f248bdd5855befa7f80e34a68286e0
StaringToSky <2897044280@qq.com>
2022-06-08 14:05:34 +0800

    Something fun

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function createArticle()
 {
     // 产生新的文章链接 （未完成）
-    var _input = document.getElementById("input-title").value
-    var _title = document.createElement("div")
-    _title.innerHTML = _input;
-    _title.className = "article-title"
-    _title.setAttribute("href","style.css")
+    //var _input = document.getElementById("input-title").value
+    //var _title = document.createElement("div")
+    //_title.innerHTML = _input;
+    //_title.className = "article-title"
+    //_title.setAttribute("href","style.css")
     
+    //document.getElementById("article").appendChild(_title)
+    var _title = document.createElement("div")
+    _title.innerHTML = "你就是家鸽吧！";
     document.getElementById("article").appendChild(_title)
 }
